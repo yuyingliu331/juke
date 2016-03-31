9f93c0b3854479a8871ddf2c513d0f7d102cb47a
Gabriel Lebec <glebec@gmail.com>
2016-03-31 17:56:25 -0400

    working seek

--- server/app/routes/songs.js
+++ server/app/routes/songs.js
@@ -29,15 +29,47 @@ router.param('songId', function (req, res, next, id) {
   .then(null, next);
 });
 
+var rangeStream = require('range-stream');
+router.use(function (req, res, next) {
+  res.seekableStream = function (stream, options) {
+    if (!options.length) {
+      var err = new Error('seekable-stream requires `length` option');
+      return next(err);
+    }
+    // indicate this resource can be partially requested
+    res.set('Accept-Ranges', 'bytes');
+    // incorporate options
+    if (options.length) res.set('Content-Length', options.length);
+    if (options.type) res.set('Content-Type', options.type);
+    // if this is a partial request
+    if (req.headers.range) {
+      // parsing request
+      var span = req.headers.range.split('=')[1].split('-');
+      var start = parseInt(span[0], 10);
+      var end = parseInt(span[1], 10) || options.length - 1;
+      // formatting response
+      res.status(206);
+      res.set('Content-Length', (end + 1) - start);
+      res.set('Content-Range', 'bytes ' + start + '-' + end + '/' + options.length);
+      // slicing the stream to partial content
+      stream = stream.pipe(rangeStream(start, end));
+    }
+    stream.pipe(res);
+  };
+  next();
+});
+
 router.get('/:songId.audio', function (req, res, next) {
   if(!req.song.extension) return next(new Error('No audio for song'));
-  res.set('Content-Type', mime.lookup(req.song.extension));
-  res.set('Content-Length', req.song.size);
-  mongoose.model('Song')
+  var options = {
+    type: mime.lookup(req.song.extension),
+    length: req.song.size
+  };
+  var stream = mongoose.model('Song')
   .findById(req.params.songId)
   .select('buffer')
-  .stream({ transform: song => song.buffer })
-  .pipe(res);
+  .stream({ transform: song => song.buffer });
+  res.seekableStream(stream, options);
 });
 
 router.get('/:songId.image', function (req, res, next) {
