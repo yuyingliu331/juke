4780800e77d4a6c3ffb2441e0ddf03daa5620770
Omri Bernstein <hippiccolo@gmail.com>
2016-03-31 18:28:10 -0400

    Fix deep populate issue

--- server/db/models/playlist.js
+++ server/db/models/playlist.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const songListPlugin = require('../plugins/songList');
 const Schema = mongoose.Schema;
+const deepPopulate = require('mongoose-deep-populate')(mongoose);
 
 const schema = new Schema({
   name: { type: String, required: true, trim: true },
@@ -10,5 +11,6 @@ const schema = new Schema({
 // with some fancy validations and auto population
 // check it out!
 schema.plugin(songListPlugin);
+schema.plugin(deepPopulate);
 
 module.exports = mongoose.model('Playlist', schema);
