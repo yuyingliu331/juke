c58b91153a72236fd5533f2f2b6041b7df7af7b6
Omri Bernstein <hippiccolo@gmail.com>
2016-05-16 11:37:19 -0400

    Fix up db-init script

--- db-init.sh
+++ db-init.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 # PostgreSQL does not supoort "IF NOT EXISTS" for db creation
 # So we use a shell script to dynamically accomplish this
 
 conditionalPsqlCreate () {
   testQuery=$1 thingToCreate=$2 tryMessage=$3
   echo $tryMessage
-  if [ $(psql -U postgres -tAc "SELECT 1 FROM $testQuery") ]; then
+  if [ $(psql -tAc "SELECT 1 FROM $testQuery") ]; then
     echo "already exists"
   else
-    psql -U postgres -c "CREATE $thingToCreate"
+    psql -c "CREATE $thingToCreate"
   fi
 }
 
